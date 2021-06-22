32c7ecfcd4d729115db99f951b3d3cc1322099ca
Konstantin Shchygolev <konstantinshchygolev@Konstantins-MacBook-Pro.local>
2021-06-22 12:34:07 +0300

    added iResipe file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { FormEvent, useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -6,21 +6,34 @@ function App() {
   const [recipesSearch, setRecipesSearch] = useState('');
 
   const searchForRecipes = async (query: string): Promise<any> => {
-    const result = await fetch(`http://www.recipepuppy.com/api/?q=${query}`);
+    const result = await fetch(`http://localhost:3001/?search=${query}`);
     return (await result.json()).results;
+  };
+
+  const search = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const input = form.querySelector('#searchText') as HTMLInputElement;
+    setRecipesSearch(input.value);
   };
 
   useEffect(() => {
     (async () => {
       const query = encodeURIComponent(recipesSearch);
-      const response = await searchForRecipes(query);
+      if(query) {
+        const response = await searchForRecipes(query);
       setRecipesFound(response);
+      }
     })();
-  });
+  }, [recipesSearch]);
 
   return (
     <div className="App">
      <h1>Recipe Search App</h1>
+     <form className="searchForm" onSubmit={event => search(event)}>
+        <input id="searchText" type="text"/>
+          <button>Search</button>
+     </form>
     </div>
   );
 }
