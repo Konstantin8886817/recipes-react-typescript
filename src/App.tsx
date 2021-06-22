f221d326eab083d56b8679f02e4e2ba86b27f94a
Konstantin Shchygolev <konstantinshchygolev@Konstantins-MacBook-Pro.local>
2021-06-22 11:49:28 +0300

    added searchForRecipes func

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [recipesFound, setRecipesFound] = useState([]);
+  const [recipesSearch, setRecipesSearch] = useState('');
+
+  const searchForRecipes = async (query: string) => {
+    const result = await fetch(`http://www.recipepuppy.com/api/?q=${query}`);
+    return (await result.json()).results;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
