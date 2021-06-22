f2589afca17db524c075fb748bd0d98ca6a4e889
Konstantin Shchygolev <konstantinshchygolev@Konstantins-MacBook-Pro.local>
2021-06-22 11:55:18 +0300

    installed axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   const [recipesFound, setRecipesFound] = useState([]);
   const [recipesSearch, setRecipesSearch] = useState('');
 
-  const searchForRecipes = async (query: string) => {
+  const searchForRecipes = async (query: string): Promise<any> => {
     const result = await fetch(`http://www.recipepuppy.com/api/?q=${query}`);
     return (await result.json()).results;
   };
 
+  useEffect(() => {
+    (async () => {
+      const query = encodeURIComponent(recipesSearch);
+      const response = await searchForRecipes(query);
+      setRecipesFound(response);
+    })();
+  });
+
   return (
     <div className="App">
-     
+     <h1>Recipe Search App</h1>
     </div>
   );
 }
